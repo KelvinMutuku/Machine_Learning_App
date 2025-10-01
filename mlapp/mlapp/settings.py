e27a7cd62bde11a960cf35a3af8a9a09fdd21597
Kelvin Mutuku <mutukuk553@gmail.com>
2025-10-01 11:10:35 +0300

    Made migrations and created a Database

--- mlapp/mlapp/settings.py
+++ mlapp/mlapp/settings.py
@@ -12,6 +12,16 @@ INSTALLED_APPS = [
     "predictor",  # <-- add
 ]
 
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',   # Required
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware', # Required
+    'django.contrib.messages.middleware.MessageMiddleware',    # Required
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -27,6 +37,13 @@ TEMPLATES = [
         },
     },
 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # For dev
 ALLOWED_HOSTS = ["*"]
