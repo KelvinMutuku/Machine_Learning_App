4503a991ceaf675ff7afd0b8ae7caa1b63774b10
Kelvin Mutuku <mutukuk553@gmail.com>
2025-10-01 11:47:16 +0300

    Made migrations and updated the file and folder paths

--- mlapp/mlapp/urls.py
+++
@@ -1,8 +0,0 @@
-# mlapp/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("predictor.urls")),  # web & API routes
-]
